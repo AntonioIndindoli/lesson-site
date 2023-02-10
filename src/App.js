70e977556080a6b7ec9bf3c40a60c2e163c1c32c
AntonioIndindoli <indindoliantonio@gmail.com>
2023-02-09 21:00:46 -0800

    updat

--- src/App.js
+++ src/App.js
@@ -19,23 +19,23 @@ function App() {
         <p className='home-info-text'>(925)-597-7979 | rodriguezcaleb.bchs@gmail.com</p>
       </div>
       <Row className="row1">
-        <Col className="column">
+        <Col className="column1">
           <img className="desc-img" src={portrait2}></img>
         </Col>
-        <Col xs={8} className="column">
+        <Col xs={8} className="column1">
           <p className='home-desc'>
-            - Tutoring
+            Tutoring
             <br></br>
             Meet Caleb Rodriguez, a multi-talented tutor who specializes in piano lessons but also offers expertise in English, spelling, math, and writing. With a passion for music and education, Caleb has dedicated himself to helping students of all ages reach their full potential.</p>
         </Col>
       </Row>
-      <Row className="row2">
-        <Col className="column">
+      <Row xs={{ gutter: 0 }}className="row2">
+        <Col className="column1">
           <img className="desc-img" src={portrait1}></img>
         </Col>
-        <Col xs={8} className="column">
+        <Col xs={8} className="column1">
           <p className='home-desc'>
-            - Piano
+            Piano
             <br></br>
             Whether you're a beginner looking to learn the basics or an experienced musician looking to perfect your skills, Caleb's personalized approach will help you achieve your goals. Book your tutoring session with Caleb today and start your child’s journey towards becoming a confident and accomplished musician or student.</p>
         </Col>
